a3a4109241045de1ce4caf4b95a03c7248366f8f
Joseph Masone <joemasone050897@gmail.com>
2023-07-16 02:05:01 -0400

    add writing to text file of frontpage items

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ doc = BeautifulSoup(result, "html.parser")
 
 frontpage_grid = doc.find(class_="frontpageGrid")
 
-frontpage_items = frontpage_grid.find_all(class_="dealCard__title")
-for item in frontpage_items:
-    print(item.text)
+file = open("frontpage.txt", "w")
+file.write("")
+file.close()
 
+frontpage_items = frontpage_grid.find_all(class_="dealCard__title")
+
+file = open("frontpage.txt", "w")
+for item in frontpage_items:
+    file.write(item.text + "\n")
+file.close()
