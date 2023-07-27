a82f96d34b79a4e31c431a55ec45b2cf17bccd17
Joseph Masone <joemasone050897@gmail.com>
2023-07-27 01:53:21 -0400

    get name, sale price, original price of all frontpage slickdeals, update gitignore

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ doc = BeautifulSoup(result, "html.parser")
 
 frontpage_grid = doc.find(class_="frontpageGrid")
 
-file = open("frontpage.txt", "w")
-file.write("")
-file.close()
 
-frontpage_items = frontpage_grid.find_all(class_="dealCard__title")
+with open("frontpage.txt", "w") as file:
+    file.write("")
 
-file = open("frontpage.txt", "w")
-for item in frontpage_items:
-    file.write(item.text + "\n")
-file.close()
+
+with open("frontpage.txt", "w") as file:
+    frontpage_items_title = frontpage_grid.find_all(class_="dealCard__title")
+    frontpage_items_price = frontpage_grid.find_all(class_="dealCard__price")
+    frontpage_items_original_price = frontpage_grid.find_all(class_="dealCard__originalPrice")
+ 
+
+    for title, price, og_price in zip(frontpage_items_title, frontpage_items_price, frontpage_items_original_price):
+        file.write(title.text + "\nSale Price: " + price.text + " \nOriginal Price: " + og_price.text + "\n\n")
+
