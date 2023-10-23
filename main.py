47785bc3b8e08de4832bee762dac25c3ce38fc56
Joseph Masone <joemasone050897@gmail.com>
2023-10-23 19:38:46 -0400

    refactor main.py, get rid of unnecessary open call, format with black

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ doc = BeautifulSoup(result, "html.parser")
 
 frontpage_grid = doc.find(class_="frontpageGrid")
 
-
-with open("frontpage.txt", "w") as file:
-    file.write("")
-
-
 with open("frontpage.txt", "w") as file:
     frontpage_items_title = frontpage_grid.find_all(class_="dealCard__title")
     frontpage_items_price = frontpage_grid.find_all(class_="dealCard__price")
-    frontpage_items_original_price = frontpage_grid.find_all(class_="dealCard__originalPrice")
- 
+    frontpage_items_original_price = frontpage_grid.find_all(
+        class_="dealCard__originalPrice"
+    )
 
-    for title, price, og_price in zip(frontpage_items_title, frontpage_items_price, frontpage_items_original_price):
-        file.write(title.text + "\nSale Price: " + price.text + " \nOriginal Price: " + og_price.text + "\n\n")
-
+    file.write("https://slickdeals.net/\n\n")
+    for title, price, og_price in zip(
+        frontpage_items_title, frontpage_items_price, frontpage_items_original_price
+    ):
+        file.write(
+            title.text
+            + "\nSale Price: "
+            + price.text
+            + " \nOriginal Price: "
+            + og_price.text
+            + "\n\n"
+        )
